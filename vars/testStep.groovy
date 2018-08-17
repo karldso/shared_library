8aeac0504722ce0fe9e698fa101c6c236ebffba7
karldso <karldso@gmail.com>
2018-08-17 10:26:45 +0300

    Update testStep.groovy

--- vars/testStep.groovy
+++ vars/testStep.groovy
@@ -1,12 +1,1 @@
-def call(body) {
-    // evaluate the body block, and collect configuration into the object
-    def config = [:]
-    body.resolveStrategy = Closure.DELEGATE_FIRST
-    body.delegate = config
-    body()
-
-
-    def commonHelpers = new commonHelpers(this)
-
-	echo "Running the JB test shared library"
-}
+echo "Running the JB test shared library"
