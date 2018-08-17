c0fa38eb26b183f989e448ebb160bf605b055f42
karldso <karldso@gmail.com>
2018-08-17 14:22:20 +0300

    Update testStep.groovy

--- vars/testStep.groovy
+++ vars/testStep.groovy
@@ -1,3 +1,4 @@
 def call(args) {
       echo "Running the JB test shared library with argument ${args}"
+      return sh(script: "echo "Running the JB test shared library with argument ${args}", returnStdout: true)
 }
